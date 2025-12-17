509b08d6247f3c05e51b2237240f2963bc01ec42
Zuhaitz-dev <200231861+Zuhaitz-dev@users.noreply.github.com>
2025-12-17 22:53:13 +0000

    Auto-update: Bump z-core and regenerate zfile.h [skip ci]

--- zfile.h
+++ zfile.h
@@ -523,7 +523,7 @@ bool zfile_is_dir(const char *path)
     {
         return false;
     }
-    return (0 != s.st_mode & S_IFDIR);
+    return (0 != (s.st_mode & S_IFDIR));
 #   else
     if (0 != stat(path, &s))
     {
